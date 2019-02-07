b8fa6d62fd7f4a2310edb8cbe0df312e4ce847f5
augustak <august@lucidtech.ai>
2019-02-07 10:40:55 +0100

    Update Added predictions on invoice and document_split

--- README.md
+++ README.md
@@ -8,6 +8,14 @@ $ pip install -r requirements.txt
 
 #### Usage
 
+Invoice prediction:
+
 ```bash
-$ python src/main.py https://xxx.api.lucidtech.ai/v1 <api_key> <access_key_id> <secret_access_key>
+$ python src/main.py https://xxx.api.lucidtech.ai/v1 <api_key> <access_key_id> <secret_access_key> invoice_prediction invoice.pdf application/pdf
+```
+
+Document split:
+
+```bash
+$ python src/main.py https://xxx.api.lucidtech.ai/v1 <api_key> <access_key_id> <secret_access_key> document_split document.pdf application/pdf
 ```
--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 import argparse
 import json
 import requests
+import pathlib
 
 from urllib.parse import urlparse
 from auth import AWSSignatureV4
 
 
-def create_auth(args):
+def create_auth():
     return AWSSignatureV4(
         region='eu-west-1',
         service='execute-api',
@@ -16,34 +17,97 @@ def create_auth(args):
     )
 
 
-def main(args):
-    auth = create_auth(args)
-    uri = urlparse(f'{args.api_endpoint}/documents')
-    body = json.dumps({'contentType': 'image/jpeg', 'consentId': '1234'}).encode()
+def create_signing_headers(method, path, body):
+    auth = create_auth()
+    uri = urlparse(f'{args.api_endpoint}{path}')
 
     auth_headers = auth.sign_headers(
         uri=uri,
-        method='POST',
+        method=method,
         body=body
     )
 
     headers = {**auth_headers, 'Content-Type': 'application/json'}
+    return uri, headers
 
-    post_documents = requests.post(
+
+def post_documents():
+    body = json.dumps({'contentType': args.content_type, 'consentId': args.consent_id}).encode()
+    uri, headers = create_signing_headers('POST', '/documents', body)
+
+    post_documents_response = requests.post(
         url=uri.geturl(),
         headers=headers,
         data=body
-    ).json()
+    )
+    post_documents_response.raise_for_status()
+    return post_documents_response.json()
 
-    print(post_documents)
+
+def put_document(presigned_url):
+    body = pathlib.Path(args.document_path).read_bytes()
+    headers = {'Content-Type': args.content_type}
+    put_document_response = requests.put(presigned_url, data=body, headers=headers)
+    put_document_response.raise_for_status()
+    return put_document_response.content.decode()
+
+
+def post_predictions(document_id, model_name):
+    body = json.dumps({'documentId': document_id, 'modelName': model_name}).encode()
+    uri, headers = create_signing_headers('POST', '/predictions', body)
+
+    post_predictions_response = requests.post(
+        url=uri.geturl(),
+        headers=headers,
+        data=body
+    )
+    post_predictions_response.raise_for_status()
+    return post_predictions_response.json()
+
+
+def upload_document():
+    post_documents_response = post_documents()
+    document_id = post_documents_response['documentId']
+    presigned_url = post_documents_response['uploadUrl']
+    put_document(presigned_url)
+    return document_id
+
+
+def invoice_prediction():
+    document_id = upload_document()
+    predictions = post_predictions(document_id, 'invoice')
+    print(json.dumps(predictions, indent=2))
+
+
+def document_split():
+    document_id = upload_document()
+    predictions = post_predictions(document_id, 'documentSplit')
+    print(json.dumps(predictions, indent=2))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('api_endpoint')
+    parser.add_argument('api_endpoint', help='HTTPS endpoint for REST API')
     parser.add_argument('api_key')
     parser.add_argument('access_key_id')
     parser.add_argument('secret_access_key')
-    args = parser.parse_args()
+    subparsers = parser.add_subparsers()
 
-    main(args)
+    invoice_prediction_parser = subparsers.add_parser('invoice_prediction')
+    invoice_prediction_parser.add_argument('document_path', help='Path to document to make predictions on')
+    invoice_prediction_parser.add_argument('content_type', choices={'image/jpeg', 'application/pdf'},
+                                           help='Content-Type of document to make predictions on')
+    invoice_prediction_parser.add_argument('--consent_id', default='1234',
+                                           help='Consent ID is typically a mapping from end user to a unique identifier')
+    invoice_prediction_parser.set_defaults(cmd=invoice_prediction)
+
+    document_split_parser = subparsers.add_parser('document_split')
+    document_split_parser.add_argument('document_path', help='Path to document to split')
+    document_split_parser.add_argument('content_type', choices={'application/pdf'},
+                                       help='Content-Type of document to split')
+    document_split_parser.add_argument('--consent_id', default='1234',
+                                       help='Consent ID is typically a mapping from end user to a unique identifier')
+    document_split_parser.set_defaults(cmd=document_split)
+
+    args = parser.parse_args()
+    args.cmd()
